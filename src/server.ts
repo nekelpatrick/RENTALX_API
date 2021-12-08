033c421909a7108ce82ce12ee7496cac6c57ea91
Patrick Nekel <patric@kenzie.com.br>
2021-12-08 12:07:46 -0300

    :sparkles: Initial commit, project begin.

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { response } from 'express'
+
+const app = express()
+
+
+app.get("/", (request, response) => {
+ return response.json({message: "Hello World"})
+})
+
+
+
+
+app.listen(3333)
