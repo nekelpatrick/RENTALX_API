339ac3d139332eaa3350179ee66bcc18317b9415
Patrick Nekel <patric@kenzie.com.br>
2021-12-08 12:15:33 -0300

    :wrench: Update configuration files

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,1 @@
 import { Request, Response } from "express";
-import CreateCourseService from "./CreateCourseService";
-
-export function createCourse(request:Request, response:Response) {
-
-	CreateCourseService.execute({
-		name: "NodeJS",
-		educator: "Patrick",
-		duration: 10
-	});
-
-	return response.send();
-}
