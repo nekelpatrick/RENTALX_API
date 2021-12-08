e72c7d8e14d182cc205b726581d83a74d32ebe83
Patrick Nekel <patric@kenzie.com.br>
2021-12-08 12:35:28 -0300

    :wrench: Update config file, add debugger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
 
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
 
 app.listen(3333, () => console.log("Server is running"));
