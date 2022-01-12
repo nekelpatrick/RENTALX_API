c5528a9a8a2fc4c744a7e1316fb0e4c09c4739a9
Patrick Nekel <nekelpatrick@pop-os.localdomain>
2022-01-12 09:53:00 -0300

    :bricks: add module folder

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { CreateCategoryService } from "../services/CreateCategoryService";
-import { PostgresCategoryRepository } from "../repositories/PostgresCategoryRepository";
-import { CategoriesRepository } from "../repositories/CategoriesRepositories";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { PostgresCategoryRepository } from "../modules/cars/repositories/PostgresCategoryRepository";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepositories";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
