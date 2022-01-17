0957cecf37bb2462f8c01d05b8fc1538ca489a6b
Patrick Nekel <nekelpatrick@pop-os.localdomain>
2022-01-17 09:56:43 -0300

    :sparkles: add specification useCase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { PostgresCategoryRepository } from "../modules/cars/repositories/PostgresCategoryRepository";
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepositories";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
