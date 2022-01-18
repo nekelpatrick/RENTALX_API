83960d4bcdfe4256cd5ceee4c7e4f42cdf9e7099
Patrick Nekel <nekelpatrick@pop-os.localdomain>
2022-01-18 11:53:12 -0300

    :memo: add swagger-ui library for documentation purposes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
+import swaggerConfigFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  "/api-documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerConfigFile)
+);
 
 app.use(router);
 
