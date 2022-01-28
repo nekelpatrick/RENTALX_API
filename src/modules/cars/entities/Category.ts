a61700ef4c07d0be7a82b0ec0c680349dd9039aa
Patrick Nekel <nekelpatrick@pop-os.localdomain>
2022-01-28 17:25:59 -0300

    :truck: rename CategoriesRepositories to CategoriesRepository

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,11 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 
 @Entity("categories")
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
